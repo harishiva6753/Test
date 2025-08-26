2b17ee2aa53e40a7fe14a6b1109ab405a54b8f58
Shivasri <harishiva6753@gmail.com>
2025-08-26 15:21:00 +0530

    first commit

---
+++ Fact.java
@@ -0,0 +1,17 @@
+package test_1;
+
+public class Fact {
+	public static int getFactorial(int n) {
+		if(n<0) 
+			return -1;
+		if(n==0)
+			return -2;
+		if(n==1)
+			return 1;
+		return n*getFactorial(n-1);
+	}
+	public static void main(String[] args) {
+		System.out.println(getFactorial(6));
+	}
+
+}
